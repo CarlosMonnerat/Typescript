d5185cd3fc5982349c4158a92f4593ba70eb0ce2
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-02-26 19:07:12 -0300

    update tuple

--- CFB_Cursos/src/aula06.ts
+++ CFB_Cursos/src/aula06.ts
@@ -1,5 +1,5 @@
 let numeros:number[] = [10, 20, 30, 40];
-//let numeros: Array<number | string> = [10, 20, 30, 40];
+//let numeros: Array <number | string> = [10, 20, 30, 40];
 //let numeros: (number | string)[] = [20, 30, 40, "Carlos"];
 
 numeros.push(50);   //Add no final do Array
