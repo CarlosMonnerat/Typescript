dcdeb98f35048273ed73038ece940eb5d8e3de18
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-02-25 22:20:50 -0300

    update

--- CFB_Cursos/build/aula06.js
+++ CFB_Cursos/build/aula06.js
@@ -1,2 +1,7 @@
 "use strict";
 let numeros = [10, 20, 30, 40];
+numeros.push(50);
+numeros.unshift(5);
+numeros.pop();
+numeros.shift();
+console.log(numeros);
--- CFB_Cursos/src/aula06.ts
+++ CFB_Cursos/src/aula06.ts
@@ -2,7 +2,13 @@ let numeros:number[] = [10, 20, 30, 40];
 //let numeros: Array<number | string> = [10, 20, 30, 40];
 //let numeros: (number | string)[] = [20, 30, 40, "Carlos"];
 
+numeros.push(50); //Add no final do Array
+numeros.unshift(5); //Add no início do Array
 
+numeros.pop(); //Retira o último elemento do Array
+numeros.shift(); //Retira o primeiro elemento do Array
+
+console.log(numeros);
 
 
 
