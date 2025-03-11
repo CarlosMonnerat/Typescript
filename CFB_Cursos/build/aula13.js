02dcb6a7b60966a87bc14504597630c04fd83b30
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-03-10 21:25:58 -0300

    update

--- CFB_Cursos/build/aula13.js
+++ CFB_Cursos/build/aula13.js
@@ -3,4 +3,9 @@ function somar(n1 = 0, n2 = 0) {
     return n1 + n2;
 }
 ;
+function novoUser(user, pass, nome) {
+    let dados = { user, pass, nome };
+    console.log(dados);
+}
 console.log(somar(5, 5));
+novoUser("carlos", "123");
