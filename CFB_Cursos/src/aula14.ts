5c128e528b1d9134cae90368ac78742683469a8c
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-03-10 21:52:36 -0300

    update

--- CFB_Cursos/src/aula14.ts
+++ CFB_Cursos/src/aula14.ts
@@ -8,8 +8,21 @@ function teste (): void {
 };
 
 //Já as Arrow Functions não não são elevadas, por tanto NÃO podem ser chamadas antes de serem criadas
- const teste2 = () => {
+ const teste2 = (): void => {
     console.log("Função 2: Tudo certo também!");
 };
 
 teste2();
+
+// Arrow Functions que soma todos os elementos de um Array
+const somaArray = (n: number[]): number => {
+    let s: number = 0;
+    n.forEach((e) =>{
+        s += e;
+    });
+    return s;
+};
+
+let numer: number[] = [10, 20, 30, 40];
+
+console.log(somaArray(numer));
