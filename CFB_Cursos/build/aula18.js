c41934de0d9e55f4b013e66d6f79144dccd02696
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-03-12 20:32:14 -0300

    update

--- CFB_Cursos/build/aula18.js
+++ CFB_Cursos/build/aula18.js
@@ -14,14 +14,23 @@ class Computador {
         console.log(`Nome..: ${this.nome}`);
         console.log(`Ram...: ${this.ram}`);
         console.log(`CPU...: ${this.cpu}`);
-        console.log(`ligado: ${this.ligado}`);
+        console.log(`ligado: ${this.ligado ? "Sim" : "Não"}`);
         console.log("------------------------");
+    }
+    ligar() {
+        this.ligado = true;
+    }
+    desligar() {
+        this.ligado = false;
     }
 }
 ;
 const comp1 = new Computador("Rapidão", 64, 10);
 const comp2 = new Computador("Carão", 32, 5);
 const comp3 = new Computador("Gamer", 128, 10);
+comp1.ligar();
+comp3.ligar();
+comp1.desligar();
 comp1.info();
 comp2.info();
 comp3.info();
--- CFB_Cursos/src/aula18.ts
+++ CFB_Cursos/src/aula18.ts
@@ -13,12 +13,20 @@ class Computador {
         this.ligado = false;
     }
 
-    info (): void{
+    info(): void{
         console.log(`Nome..: ${this.nome}`);
         console.log(`Ram...: ${this.ram}`);
         console.log(`CPU...: ${this.cpu}`);
-        console.log(`ligado: ${this.ligado}`);
+        console.log(`ligado: ${this.ligado ? "Sim" : "Não"}`);
         console.log("------------------------");
+    }
+
+    ligar(): void {
+        this.ligado = true;
+    }
+
+    desligar(): void {
+        this.ligado = false;
     }
 };
 
@@ -26,6 +34,10 @@ class Computador {
 const comp1 = new Computador("Rapidão", 64, 10);
 const comp2 = new Computador("Carão", 32, 5);
 const comp3 = new Computador("Gamer", 128, 10);
+
+comp1.ligar();
+comp3.ligar();
+comp1.desligar();
 
 comp1.info();
 comp2.info();
