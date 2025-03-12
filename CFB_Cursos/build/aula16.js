5f66f601fb760ca5adc797cdcec9479351bb4512
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-03-12 19:47:55 -0300

    create aula16.ts

---
+++ CFB_Cursos/build/aula16.js
@@ -0,0 +1,12 @@
+"use strict";
+class Computador {
+    nome = "";
+    ram = 0;
+    cpu = 0;
+    ligado = false;
+}
+;
+const c1 = new Computador();
+const c2 = new Computador();
+const c3 = new Computador();
+console.log(c1.ram);
---
+++ CFB_Cursos/src/aula16.ts
@@ -0,0 +1,14 @@
+//P.O.O Classes
+
+class Computador {
+    nome: string = "";
+    ram: number = 0;
+    cpu: number = 0;
+    ligado: boolean = false;
+};
+
+const c1 = new Computador();
+const c2 = new Computador();
+const c3 = new Computador();
+
+console.log(c1.ram);
