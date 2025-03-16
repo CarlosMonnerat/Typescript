cdd36543dcc5e9b6a502d2b317cc303fb486124c
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-03-15 22:20:04 -0300

    create aula20.ts

---
+++ CFB_Cursos/build/aula20.js
@@ -0,0 +1,11 @@
+"use strict";
+class Conta {
+    numero;
+    titular;
+    constructor(numero, titular) {
+        this.numero = numero;
+        this.titular = titular;
+    }
+    ;
+}
+;
---
+++ CFB_Cursos/src/aula20.ts
@@ -0,0 +1,12 @@
+// HERANÇA
+
+class Conta {
+    numero: number;
+    titular: string;
+    
+    constructor(numero: number, titular: string){
+        this.numero = numero;
+        this.titular = titular;
+    };
+};
+
