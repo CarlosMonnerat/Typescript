ee1c9119ee944a311b843517e7aa5a33d8c0f5fa
carlos <carlosh.monnerat@gmail.com>
2025-03-18 19:58:06 -0300

    update

--- CFB_Cursos/src/aula28.ts
+++ CFB_Cursos/src/aula28.ts
@@ -1,16 +1,20 @@
-// Interface e Objetos Literias
+// Interface e Funções
 interface curso {
     titulo: string;
     des: string;
     numAula: number;
     maxAlunos ?: number;    //'?' Parrâmetro opcional
+    inicarCurso ?(teste: string): void;
  };
+
+ 
 
  let curso_4: curso;
  let curso_5: curso;
  let curso_6: curso;
 
- curso_4 = { titulo: "TypeScript", des: "Curso de Typescript para iniciantes", numAula: 100, maxAlunos: 50 };
+
+ curso_4 = { titulo: "TypeScript", des: "Curso de Typescript para iniciantes", numAula: 100, maxAlunos: 50, inicarCurso(t: string){}};
  curso_5 = { titulo: "JavaScript", des: "Curso de Javascript puro", numAula: 200, maxAlunos: 80 };
  curso_6 = { titulo: "C++", des: "Curso de C++ Avançado", numAula: 70 };
 
