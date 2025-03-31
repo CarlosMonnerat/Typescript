640aa2cbe4490e09b3a1ec823bd318a3642e1fc3
carlos <carlosh.monnerat@gmail.com>
2025-03-31 20:48:20 -0300

    module

---
+++ CFB_Cursos/build/aula30.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Classes_1 = require("./Classes");
+const p1 = new Classes_1.Pessoa("Carlos", 1.65);
+const p2 = new Classes_1.Objeto("Mesa");
+console.log(p1.nome);
+console.log(p1.altura);
+console.log(p2.nome);
---
+++ CFB_Cursos/src/aula30.ts
@@ -0,0 +1,9 @@
+import {Pessoa, Objeto} from "./Classes";
+//import {Objeto} from "./Classes";
+
+const p1 = new Pessoa("Carlos", 1.65);
+const p2 = new Objeto("Mesa");
+
+console.log(p1.nome);
+console.log(p1.altura);
+console.log(p2.nome);
