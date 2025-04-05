a5fb05e9ae27609c6e8e907c8a6dbfcd6155b341
carlos <carlosh.monnerat@gmail.com>
2025-04-04 22:16:38 -0300

    update

--- CFB_Cursos/build/aula32.js
+++ CFB_Cursos/build/aula32.js
@@ -6,11 +6,10 @@ var Veiculos;
         motor;
         constructor(nome) {
             this.nome = nome;
-            this.motor = new Motores.Motor(100);
+            this.motor = new Motores.Motor(3, 100);
         }
     }
     Veiculos.Carro = Carro;
-    ;
 })(Veiculos || (Veiculos = {}));
 ;
 var Motores;
@@ -20,15 +19,29 @@ var Motores;
         constructor(pot) {
             this.pot = pot;
         }
+        get potencia() {
+            return this.pot;
+        }
     }
-    ;
     class Motor {
+        ligado;
+        cilindros;
         pot;
-        constructor(pot) {
-            this.pot = pot;
+        constructor(cilindros, pot, turbo) {
+            this.ligado = false;
+            this.cilindros = cilindros;
+            this.pot = pot + (turbo ? turbo.potencia : 0);
+        }
+        set liga(ligado) {
+            this.ligado = ligado;
+        }
+        get liga() {
+            return this.ligado;
+        }
+        get potencia() {
+            return this.pot;
         }
     }
     Motores.Motor = Motor;
-    ;
 })(Motores || (Motores = {}));
 ;
