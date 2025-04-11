901bc29741b72e376a7e735bc46162d0c24cac20
carlos <carlosh.monnerat@gmail.com>
2025-04-11 19:29:07 -0300

    update

--- CFB_Cursos/src/aula35.ts
+++ CFB_Cursos/src/aula35.ts
@@ -3,17 +3,32 @@
 let av = [10,20,30,40];
 
 //Sem desestruturação:
-// let aa, bb, cc, dd;
-//     aa = av[0];
-//     bb = av[1];
-//     cc = av[2];
+// let a, b, c, dd;
+//     a = av[0];
+//     b = av[1];
+//     c = av[2];
 //     dd = av[3];
 
 
 //Usando a desestruturação:
-let [aa, bb, cc, dd] = av;
+let [a, b, c, dd] = av;
 
-console.log(aa);
-console.log(bb);
-console.log(cc);
+console.log(a);
+console.log(b);
+console.log(c);
 console.log(dd);
+console.log("---------------");
+
+//Variações:
+let [e, f, g, h] = [50, 60, 70, 80];
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log("---------------");
+
+let [i, j, k, l] = ["vermelho", "amarelo", "azul", "verde"];
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
