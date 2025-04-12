f200df4b6290b224e599f2045299dee28a227ee1
carlos <carlosh.monnerat@gmail.com>
2025-04-12 17:09:15 -0300

    update

--- CFB_Cursos/build/Globais.js
+++ CFB_Cursos/build/Globais.js
@@ -3,6 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Globais = void 0;
 class Globais {
     static teste = 0;
+    static arrayTeste = [5, 4, 3, 2, 1];
+    static objeto = {
+        nome: "Carlos",
+        canal: "CFB Cursos"
+    };
 }
 exports.Globais = Globais;
 ;
--- CFB_Cursos/src/aula37.ts
+++ CFB_Cursos/src/aula37.ts
@@ -5,7 +5,10 @@ import { Globais } from "./Globais";
 //const g1 = new Globais(); Não é necessário instanciar 
 
 console.log(Globais.teste);
+console.log(Globais.arrayTeste);
+console.log(Globais.objeto.nome);
+console.log(Globais.objeto.canal);
 
+//Alterando os valores
 Globais.teste = 10;
-
 console.log(Globais.teste);
