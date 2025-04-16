1ab6a64d501b71b1317d22f9c840c39cd5ba7d34
carlos <carlosh.monnerat@gmail.com>
2025-04-16 17:31:24 -0300

    update

--- React-NextJS/projeto01/projeto01/src/pages/index.tsx
+++ React-NextJS/projeto01/projeto01/src/pages/index.tsx
@@ -16,9 +16,12 @@ let canal = 'CFBCursos';
 
 function Topo(){
   return (
-    <div>
+    <div className='flex justify-between items-center bg-zinc-300 h-[100px]'>
       <div>Logo</div>
-      <div>{canal}</div>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='text-3xl'>{canal}</div>
+        <div>Curso de React</div>
+      </div>
       <div>{nome}</div>
     </div>
   );
