ad67b83b2e800f0bc6dfa90ce8478b68f6991658
carlos <carlosh.monnerat@gmail.com>
2025-04-16 18:22:45 -0300

    Update index.tsx

--- React-NextJS/projeto01/projeto01/src/pages/index.tsx
+++ React-NextJS/projeto01/projeto01/src/pages/index.tsx
@@ -31,9 +31,21 @@ export default function Home() {
   return (
     <main>
       <Topo/>
-      <div>Curso de React</div>
-      <div>TypeScript</div>
-      <div>Next jS</div>
+      <div style={testecss}>
+        <div>Curso de React</div>
+        <div>TypeScript</div>
+        <div style={{color: '#f00', backgroundColor:'#bbb'}}>Next jS</div>
+      </div>  
     </main>
   );
+}
+
+const testecss = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItens: 'center',
+  gap: '50px',
+  color: '#00f',
+  backgroundColor: '#eee',
+  fontSize: '20px'
 }
