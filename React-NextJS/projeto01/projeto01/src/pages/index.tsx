08538be6a249c9520a9874b6f099ed4b00a18609
carlos <carlosh.monnerat@gmail.com>
2025-04-17 12:20:38 -0300

    create component

--- React-NextJS/projeto01/projeto01/src/pages/index.tsx
+++ React-NextJS/projeto01/projeto01/src/pages/index.tsx
@@ -1,31 +1,4 @@
-// import Image from "next/image";
-// import { Geist, Geist_Mono } from "next/font/google";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-const nome = 'Carlos Quintanilha';
-let canal = 'CFBCursos';
-
-function Topo(){
-  return (
-    <div className='flex justify-between items-center bg-zinc-300 h-[100px]'>
-      <div>Logo</div>
-      <div className='flex flex-col justify-center items-center'>
-        <div className='text-3xl'>{canal}</div>
-        <div className="subtitulo-topo">Curso de React</div>
-      </div>
-      <div>{nome}</div>
-    </div>
-  );
-}
+import Topo from "@/components/Topo";
 
 export default function Home() {
   return (
