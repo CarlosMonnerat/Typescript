f48c93d94da5b2751dc8984048f29f345041b30a
carlos <carlosh.monnerat@gmail.com>
2025-04-17 14:05:17 -0300

    update

--- React-NextJS/projeto01/projeto01/src/components/Card.tsx
+++ React-NextJS/projeto01/projeto01/src/components/Card.tsx
@@ -1,8 +1,16 @@
-export default function Card(props: any){
+interface CardProps{
+    produto: string,
+    valor: number,
+    desconto: number
+}
+
+export default function Card(props: CardProps){
     return(
         <div className='flex flex-col items-center border border-red-700 rounded-sm p-1'>
-            <div>{props.produto}</div>
-            <div>{props.valor}</div>
+            <div>Produto: {props.produto}</div>
+            <div>Valor: R${props.valor}</div>
+            <div>Desconto: R${props.desconto}</div>
+            <div>Preço de venda: R${props.valor - props.desconto}</div>
         </div>
     );
 }
