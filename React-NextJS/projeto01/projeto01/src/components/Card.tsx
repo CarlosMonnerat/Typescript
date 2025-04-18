27c9e2dd543c5d6d8cdbc37ccd4145a89da0bac9
carlos <carlosh.monnerat@gmail.com>
2025-04-18 13:34:46 -0300

    update

--- React-NextJS/projeto01/projeto01/src/components/Card.tsx
+++ React-NextJS/projeto01/projeto01/src/components/Card.tsx
@@ -10,8 +10,12 @@ export default function Card(props: CardProps){
         <div className='flex flex-col items-center border border-red-700 rounded-sm p-1'>
             <div>Produto: {props.produto}</div>
             <div>Valor: R${props.valor}</div>
-            <div>Desconto: R${props.desconto}</div>
-            <div>Preço de venda: R${props.funcao(props.valor, props.desconto)}</div>
+            {props.desconto > 0 ? (
+                <div>
+                    <div>Desconto: R${props.desconto}</div>
+                    <div>Preço de venda: R${props.funcao(props.valor, props.desconto)}</div>
+                </div>
+            ) : ""}    
         </div>
     );
 }
--- React-NextJS/projeto01/projeto01/src/pages/index.tsx
+++ React-NextJS/projeto01/projeto01/src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <div className='flex justify-center gap-3'>
         <Card produto={'Mouse'} valor={49.90} desconto={10} funcao={calcDesc}/> 
         <Card produto={'Teclado'} valor={69.90} desconto={0} funcao={calcDesc}/> 
-        <Card produto={'Monitor'} valor={459.90} desconto={5} funcao={calcDesc2}/>  
+        <Card produto={'Monitor'} valor={459.90} desconto={0} funcao={calcDesc2}/>  
         <Card produto={'CPU'} valor={799.90} desconto={50} funcao={calcDesc2}/>  
       </div> 
     </main>
