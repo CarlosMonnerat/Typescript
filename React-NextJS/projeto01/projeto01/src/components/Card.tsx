63ffbe0e0b493a0525345a3d95afa3ba2c116bed
carlos <carlosh.monnerat@gmail.com>
2025-04-18 13:37:48 -0300

    Update Card.tsx

--- React-NextJS/projeto01/projeto01/src/components/Card.tsx
+++ React-NextJS/projeto01/projeto01/src/components/Card.tsx
@@ -10,12 +10,12 @@ export default function Card(props: CardProps){
         <div className='flex flex-col items-center border border-red-700 rounded-sm p-1'>
             <div>Produto: {props.produto}</div>
             <div>Valor: R${props.valor}</div>
-            {props.desconto > 0 ? (
+            {props.desconto > 0 && (
                 <div>
                     <div>Desconto: R${props.desconto}</div>
                     <div>Preço de venda: R${props.funcao(props.valor, props.desconto)}</div>
                 </div>
-            ) : ""}    
+            )}    
         </div>
     );
 }
