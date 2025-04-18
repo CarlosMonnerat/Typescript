47cffd501f85d7f783d7969af9e352f4c079d954
carlos <carlosh.monnerat@gmail.com>
2025-04-18 17:13:41 -0300

    update

--- React-NextJS/projeto01/projeto01/src/components/Topo.tsx
+++ React-NextJS/projeto01/projeto01/src/components/Topo.tsx
@@ -1,15 +1,32 @@
+import Link from "next/link";
+
 export default function Topo(){
     return (
-    <div className='flex justify-between items-center bg-zinc-300 h-[100px]'>
+    <div className='flex flex-col justify-between items-center bg-zinc-300 h-[135px]'>
         <div>Logo</div>
             <div className='flex flex-col justify-center items-center'>
             <div className='text-3xl'>CfB Cursos</div>
             <div className="subtitulo-topo">Curso de React</div>    
         </div>
         <nav>
-            Carlos
+            <Link href={"/"}>
+                Home
+            </Link>
+            <Link href={ 
+                {
+                    pathname: "/produtos/produtos",
+                    query: {
+                        nome: 'Carlos', curso: 'React Next'
+                    }
+                }
+            }>
+                Produtos
+            </Link>
+            <Link href={"/teste/test"}>
+                Teste
+            </Link>
         </nav>
     </div>
 
     );
-}
+}35
--- React-NextJS/projeto01/projeto01/src/pages/produtos/produtos.tsx
+++ React-NextJS/projeto01/projeto01/src/pages/produtos/produtos.tsx
@@ -1,5 +1,6 @@
 import Topo from "@/components/Topo";
 import Card from "@/components/Card";
+import { useRouter } from "next/router";
 
 function calcDesc(v: number, d: number){
    return v - d;
@@ -11,36 +12,42 @@ function calcDesc2(v: number, d: number){
 
 const produtos = [
    {
+     id: 1, 
      produto: 'Mouse',
      valor: 49.90,
      desconto: 0,
      disponivel: true
    },
    {
+     id: 2, 
      produto: 'Teclado',
      valor: 69.90,
      desconto: 10,
      disponivel: true
    },
    {
+     id: 3, 
      produto: 'Monitor',
      valor: 659.90,
      desconto: 0,
      disponivel: true
    },
    {
+     id: 4,
      produto: 'CPU',
      valor: 829.90,
      desconto: 100,
      disponivel: true
    },
    {
+     id: 5,
      produto: 'Caixa de Som',
      valor: 39.90,
      desconto: 0,
      disponivel: true
    },
    {
+     id: 6,
      produto: 'Microfone',
      valor: 65.90,
      desconto: 0,
@@ -49,6 +56,12 @@ const produtos = [
 ];
 
 export default function ProdutosPagina(){
+   const router = useRouter()
+   //const nome = router.query.nome;
+   //const curso = router.query.curso;
+   const {nome, curso} = router.query;
+   console.log(nome);
+   console.log(curso);
    return(
       <div>
          <Topo/>
@@ -57,7 +70,7 @@ export default function ProdutosPagina(){
                produtos.map( (e: any) => {
                   if(e.disponivel){
                   return(
-                     <Card produto={e.produto} valor={e.valor} desconto={e.desconto} funcao={calcDesc}/>
+                     <Card key={e.id} produto={e.produto} valor={e.valor} desconto={e.desconto} funcao={calcDesc}/>
                   )}
                })
             } 
