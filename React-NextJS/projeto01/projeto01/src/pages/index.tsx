a1cb978850ae79d48cd6fda7f967a599a9f3d95b
carlos <carlosh.monnerat@gmail.com>
2025-04-18 14:07:30 -0300

    update

--- React-NextJS/projeto01/projeto01/src/pages/index.tsx
+++ React-NextJS/projeto01/projeto01/src/pages/index.tsx
@@ -1,6 +1,45 @@
 import Topo from "@/components/Topo";
 import Card from "@/components/Card";
 
+const produtos = [
+  {
+    produto: 'Mouse',
+    valor: 49.90,
+    desconto: 0,
+    disponivel: true
+  },
+  {
+    produto: 'Teclado',
+    valor: 69.90,
+    desconto: 10,
+    disponivel: true
+  },
+  {
+    produto: 'Monitor',
+    valor: 659.90,
+    desconto: 0,
+    disponivel: true
+  },
+  {
+    produto: 'CPU',
+    valor: 829.90,
+    desconto: 100,
+    disponivel: true
+  },
+  {
+    produto: 'Caixa de Som',
+    valor: 39.90,
+    desconto: 0,
+    disponivel: true
+  },
+  {
+    produto: 'Microfone',
+    valor: 65.90,
+    desconto: 0,
+    disponivel: true
+  }
+];
+
 function calcDesc(v: number, d: number){
   return v - d;
 }
@@ -8,8 +47,6 @@ function calcDesc(v: number, d: number){
 function calcDesc2(v: number, d: number){
   return v - (d/2);
 }
-
-
 
 export default function Home() {
   return (
@@ -21,10 +58,15 @@ export default function Home() {
         <div style={{color: '#f00', backgroundColor:'#bbb'}}>Next jS</div>
       </div>
       <div className='flex justify-center gap-3'>
-        <Card produto={'Mouse'} valor={49.90} desconto={0} funcao={calcDesc}/> 
-        <Card produto={'Teclado'} valor={69.90} desconto={0} funcao={calcDesc}/> 
-        <Card produto={'Monitor'} valor={459.90} desconto={100} funcao={calcDesc2}/>  
-        <Card produto={'CPU'} valor={799.90} desconto={0} funcao={calcDesc2}/>  
+        {
+          produtos.map( (e: any) => {
+            if(e.disponivel){
+              return(
+                <Card produto={e.produto} valor={e.valor} desconto={e.desconto} funcao={calcDesc}/>
+              )
+            }
+          })
+        } 
       </div> 
     </main>
   );
