0382fecf95094ba76576285641e1774af19b712e
carlos <carlosh.monnerat@gmail.com>
2025-04-23 18:44:58 -0300

    update

--- React-NextJS/projeto01/projeto01/src/components/DisplayState.tsx
+++ React-NextJS/projeto01/projeto01/src/components/DisplayState.tsx
@@ -1,18 +1,30 @@
 interface DisplayStateProps{
-   valor: number
-   add: any,
-   rem: any
+   valor: number,
+   func_state: any
 }
 
 export default function DisplayState(props: DisplayStateProps){
+
+   function adicionar(){
+      let novoValor = props.valor;
+      novoValor++;
+      props.func_state(novoValor);
+   }
+
+   function remover(){
+      let novoValor = props.valor;
+      novoValor--;
+      props.func_state(novoValor);
+   }
+
    return(
       <div className=" bg-zinc-300 flex flex-col justify-center items-center p-5 m-2.5 rounded-md w-[300px]">
          <div className="bg-zinc-700 flex justify-center items-center p-1.5 mb-3 rounded-md w-[50px] text-green-300" >
             {props.valor}
          </div>
          <div className="flex gap-5">
-            <button className="btnPadrão" onClick={props.add}>Adicionar</button>
-            <button className="btnPadrão" onClick={props.rem}>Remover</button>
+            <button className="btnPadrão" onClick={adicionar}>Adicionar</button>
+            <button className="btnPadrão" onClick={remover}>Remover</button>
          </div> 
       </div>
    )
--- React-NextJS/projeto01/projeto01/src/pages/usestate/usestate.tsx
+++ React-NextJS/projeto01/projeto01/src/pages/usestate/usestate.tsx
@@ -4,23 +4,10 @@ import DisplayState from "@/components/DisplayState";
 export default function usestate(){
    const [cont, setCont] = useState<number>(0)
 
-   function adicionar(){
-      let novoValor = cont;
-      novoValor++;
-      setCont(novoValor);
-   }
-
-   function remover(){
-      let novoValor = cont;
-      novoValor--;
-      setCont(novoValor);
-   }
-
-
    return(
       <div className="flex flex-col items-center p-5">
          Página useState!!!
-         <DisplayState valor={cont} add={adicionar} rem={remover}></DisplayState>  
+         <DisplayState valor={cont} func_state={setCont}></DisplayState>  
       </div>
    )   
 }
