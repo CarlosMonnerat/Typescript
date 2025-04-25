db7ab0eb1540db4ae6524552e193c16ad6f116f5
carlos <carlosh.monnerat@gmail.com>
2025-04-25 19:05:22 -0300

    update

--- React-NextJS/projeto01/projeto01/src/pages/inputs/inputs.tsx
+++ React-NextJS/projeto01/projeto01/src/pages/inputs/inputs.tsx
@@ -1,7 +1,10 @@
 export default function inputs(){
    return(
       <div>
-         
+         <div className="campoForm m-2.5">
+            <label>Nome</label>
+            <input type="text"/>
+         </div>
       </div>
    )
 }
