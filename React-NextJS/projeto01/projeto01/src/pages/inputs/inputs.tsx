3042cc0d555f229aeaeb7f8ff9d55b029bde65ac
carlos <carlosh.monnerat@gmail.com>
2025-04-25 19:44:32 -0300

    Update inputs.tsx

--- React-NextJS/projeto01/projeto01/src/pages/inputs/inputs.tsx
+++ React-NextJS/projeto01/projeto01/src/pages/inputs/inputs.tsx
@@ -10,18 +10,25 @@ export default function inputs(){
          <Topo/>
          <div className="m-2.5">
             <div className="campoForm">
-               <label>Nome</label>
-               <input type="text" value={nome} onChange={(evt) => setNome(evt.target.value)}/>
+                  <label>Nome</label>
+                  <input type="text" value={nome} onChange={(evt) => setNome(evt.target.value)}/>
             </div>
             <div className="campoForm">
                <label>Curso</label>
-               <input type="text" value={curso} onChange={(evt) => setCurso(evt.target.value)}/>
+               <select value={curso} onChange={(evt) => setCurso(evt.target.value)}>
+                  <option value=""></option>
+                  <option value="HTML">HTML</option>
+                  <option value="React">React</option>
+                  <option value="C++">C++</option>
+                  <option value="JavaScript">JavaScript</option>
+                  <option value="Arduino">Arduino</option>
+                  <option value="CSS">CSS</option>
+               </select>
             </div>
 
-            <div>Nome digitado: {nome}</div>
+            <div>Nome Digitadoo: {nome}</div>
             <div>Curso escolhido: {curso}</div>
          </div>
-      </div>
-      
+      </div>   
    )
 }
