9ea931c34fa863a29294cf01685cce6ed34a3bfb
carlos <carlosh.monnerat@gmail.com>
2025-04-25 19:22:29 -0300

    Update inputs.tsx

--- React-NextJS/projeto01/projeto01/src/pages/inputs/inputs.tsx
+++ React-NextJS/projeto01/projeto01/src/pages/inputs/inputs.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
+
 export default function inputs(){
+   const [nome, setNome] = useState<string>("");
+   const [curso, setCurso] = useState<string>("");
+
    return(
-      <div>
-         <div className="campoForm m-2.5">
+      <div className="m-2.5">
+         <div className="campoForm">
             <label>Nome</label>
-            <input type="text"/>
+            <input type="text" value={nome} onChange={(evt) => setNome(evt.target.value)}/>
          </div>
+         <div className="campoForm">
+            <label>Curso</label>
+            <input type="text" value={curso} onChange={(evt) => setCurso(evt.target.value)}/>
+         </div>
+         
+         <div>Nome digitado: {nome}</div>
+         <div>Curso escolhido: {curso}</div>
       </div>
    )
 }
