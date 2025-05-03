c4e6fa14139d84808baa660ba13f3a54b30acc4b
carlos <carlosh.monnerat@gmail.com>
2025-05-03 18:46:00 -0300

    Update useEffect.tsx

--- React-NextJS/projeto01/projeto01/src/pages/useEffect/useEffect.tsx
+++ React-NextJS/projeto01/projeto01/src/pages/useEffect/useEffect.tsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react"
 import Topo from "@/components/Topo"
 
 export default function UseEffect(){
+   const [cont, setCont] = useState<number>(0);
+
+   useEffect(()=>{
+
+   });
+
+   function add(){
+      let c = cont;
+      c++;
+      setCont(c);
+   }
+
    return(
       <div>
          <Topo/>
-         <h1>Página useEffect!!!</h1>
+         <h1>Página useEffect</h1>
+         <div>
+            <p>{`Valor de cont: ${cont}`}</p>
+            <button className="btnPadrão" onClick={add}>Adicionar</button>
+         </div>   
       </div>
    )
-}
+};
