9cb1ff73329111dc5a60f3bf1b4f0386e8e35a2c
carlos <carlosh.monnerat@gmail.com>
2025-05-05 18:53:05 -0300

    Update usestate.tsx

--- React-NextJS/projeto01/projeto01/src/pages/usestate/usestate.tsx
+++ React-NextJS/projeto01/projeto01/src/pages/usestate/usestate.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import DisplayState from "@/components/DisplayState";
 import Topo from "@/components/Topo";
 
-export default function usestate(){
+export default function Usestate(){
    const [cont, setCont] = useState<number>(0)
 
    return(
