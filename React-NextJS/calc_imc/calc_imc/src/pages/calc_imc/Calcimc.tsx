8311d3340d4b3278610df6cbac65ce8e1caef65b
carlos <carlosh.monnerat@gmail.com>
2025-05-16 16:33:56 -0300

    update

--- React-NextJS/calc_imc/calc_imc/src/pages/calc_imc/Calcimc.tsx
+++ React-NextJS/calc_imc/calc_imc/src/pages/calc_imc/Calcimc.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Form from "@/components/Form";
 
 export default function Calcimc () {
    const [peso, setPeso] = useState<string>('');
@@ -7,6 +8,8 @@ export default function Calcimc () {
    return (
       <div>
          <p>Cálculo do IMC</p>
+         <Form label = "Peso" state={peso} funcState={setPeso}/>
+         <Form label = "Altura" state={altura} funcState={setAltura}/>
       </div>
    )
 };
