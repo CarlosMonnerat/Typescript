c72cbf33ef7d93a6886d88595d5440c2ec5f9acd
carlos <carlosh.monnerat@gmail.com>
2025-05-16 16:15:31 -0300

    create project

---
+++ React-NextJS/calc_imc/calc_imc/src/pages/calc_imc/Calcimc.tsx
@@ -0,0 +1,7 @@
+export default function Calcimc () {
+   return (
+      <div>
+         <p>Cálculo do IMC</p>
+      </div>
+   )
+};
---
+++ React-NextJS/calc_imc/calc_imc/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Calcimc from "./calc_imc/Calcimc";
+
+export default function Home() {
+  return (
+    <div className="flex w-full h-screen justify-start items-start bg-amber-200">
+      <Calcimc/>
+    </div>
+  )
+};
