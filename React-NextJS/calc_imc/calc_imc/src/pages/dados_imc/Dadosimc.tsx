c2f4b6bcd15a4db5f8b35f2ca0d0b2ee0720c5ff
carlos <carlosh.monnerat@gmail.com>
2025-05-17 16:53:27 -0300

    Update Dadosimc.tsx

--- React-NextJS/calc_imc/calc_imc/src/pages/dados_imc/Dadosimc.tsx
+++ React-NextJS/calc_imc/calc_imc/src/pages/dados_imc/Dadosimc.tsx
@@ -1,4 +1,12 @@
+import {useRouter} from 'next/router';
+
 export default function Dadosimc () {
+   const router = useRouter();
+   const {p_peso, p_altura, p_imc} = router.query;
+   console.log(p_peso)
+   console.log(p_altura)
+   console.log(p_imc)
+
    return (
       <div>
          Dados IMC
