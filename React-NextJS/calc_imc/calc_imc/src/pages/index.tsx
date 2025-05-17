ed9d3d1d7cfadadb429be8c50818b83cc7299a34
carlos <carlosh.monnerat@gmail.com>
2025-05-17 15:10:58 -0300

    Update index.tsx

--- React-NextJS/calc_imc/calc_imc/src/pages/index.tsx
+++ React-NextJS/calc_imc/calc_imc/src/pages/index.tsx
@@ -2,7 +2,7 @@ import Calcimc from "./calc_imc/Calcimc";
 
 export default function Home() {
   return (
-    <div  className="flex w-full h-screen justify-start items-start bg-amber-200">
+    <div  className="flex w-full h-screen justify-center items-start bg-amber-200">
       <Calcimc/>
     </div>
   )
