4f7d2f67ddeee0de68b7f1c8a5302bcd9d65f4a2
carlos <carlosh.monnerat@gmail.com>
2025-06-06 14:59:14 -0300

    Update Page.tsx

--- React-NextJS/E-Commerce/ecommerce/src/components/template/Page.tsx
+++ React-NextJS/E-Commerce/ecommerce/src/components/template/Page.tsx
@@ -7,9 +7,11 @@ export interface PageProps {
 
 export default function Page(props: PageProps) {
    return(
-      <div className="flex flex-col">
+      <div className="flex flex-col min-h-screen">
          <Header/>
-         <main>
+         <main className={`bg-amber-500 w-[1200px] mx-auto flex-1
+            ${props.className ?? ''} py-10`
+         }>
             {props.children}
          </main>
       </div>
