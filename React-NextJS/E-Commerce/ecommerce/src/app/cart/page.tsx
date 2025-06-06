1923baa5c1f34c9295a29f8dd348fb32aed7865b
carlos <carlosh.monnerat@gmail.com>
2025-06-06 15:09:56 -0300

    Update page.tsx

--- React-NextJS/E-Commerce/ecommerce/src/app/cart/page.tsx
+++ React-NextJS/E-Commerce/ecommerce/src/app/cart/page.tsx
@@ -1,9 +1,15 @@
+'use client'
 import Page from "@/components/template/Page";
+import { useState } from "react";
 
 export default function CartPage() {
+   const [num, setNum] = useState(0);
+
    return (
       <Page>
-         <div>Carrinho</div>
+         <div>{num}</div>
+         <button onClick ={() => setNum(num +1)}>ADD</button>
+         <button onClick ={() => setNum(num -1)}>Remove</button>
       </Page>
    )
 };
