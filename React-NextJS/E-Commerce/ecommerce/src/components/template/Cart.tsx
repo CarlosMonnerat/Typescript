ef1a62cbe4b8c0dfefcf9b9f9d028e4c8723dee9
carlos <carlosh.monnerat@gmail.com>
2025-06-06 14:39:22 -0300

    Update Cart.tsx

--- React-NextJS/E-Commerce/ecommerce/src/components/template/Cart.tsx
+++ React-NextJS/E-Commerce/ecommerce/src/components/template/Cart.tsx
@@ -1,9 +1,13 @@
+import { IconShoppingCart } from "@tabler/icons-react";
 import Link from "next/link";
 
 export default function Cart() {
    return (
       <Link href = "/cart">
-         Carrinho
+         <div className="flex relative">
+            <IconShoppingCart size={32} stroke={1}/>
+            <div className="absolute w-5 h-5 bg-red-600 rounded-full -top-2 -right-2 flex justify-center items-center text-xs">0</div>
+         </div>
       </Link>
    )
 };
