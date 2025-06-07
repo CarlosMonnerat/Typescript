1c0a8550ed5e59bd31acda0e0a6f16eb6d39ce59
carlos <carlosh.monnerat@gmail.com>
2025-06-07 16:33:32 -0300

    Update ProductCard.tsx

--- React-NextJS/E-Commerce/ecommerce/src/components/product/ProductCard.tsx
+++ React-NextJS/E-Commerce/ecommerce/src/components/product/ProductCard.tsx
@@ -9,9 +9,17 @@ export default function ProductCard(props: ProductCardProps) {
    const { id, name, description, price, image} = props.product;
 
    return (
-      <div className="flex flex-col w-72">
+      <div className="flex flex-col w-72 bg-zinc-400">
          <div className="relative w-72 h-52">
-            <Image src={image} alt={name} fill />
+            <Image src={image} alt={name} fill className="object-cover" />
+         </div>
+         <div className="flex-1">
+            <h2 className="text-xl font-bold">{name}</h2>
+            <p className="flex-1 text-sm text-zinc-900">{description}</p>
+            <div className="flex justify-between items-center">
+               <span className="text-lg font-semibold mt-2">R${price.toFixed(2)}</span>
+               <button>Adicionar</button>
+            </div>
          </div>
       </div>
    )
