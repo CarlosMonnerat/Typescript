408835fb319f1de6ec3e14857a84be6bb88f45bf
carlos <carlosh.monnerat@gmail.com>
2025-06-07 14:57:42 -0300

    Update layout.tsx

--- React-NextJS/E-Commerce/ecommerce/src/app/(store)/layout.tsx
+++ React-NextJS/E-Commerce/ecommerce/src/app/(store)/layout.tsx
@@ -4,7 +4,7 @@ import { ProviderCart } from "@/data/context/cartContext";
 export default function Layout(props: any) {
    return(
       <ProviderCart>
-         <div className="border-3 border-blue-900">
+         <div className="border-5 border-red-500">
             {props.children}
          </div>
       </ProviderCart>
