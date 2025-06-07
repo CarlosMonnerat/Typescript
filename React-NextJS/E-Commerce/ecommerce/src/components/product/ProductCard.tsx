ac179e01751078b13c2834513974d307bf763c23
carlos <carlosh.monnerat@gmail.com>
2025-06-07 15:12:25 -0300

    Create ProductCard.tsx

---
+++ React-NextJS/E-Commerce/ecommerce/src/components/product/ProductCard.tsx
@@ -0,0 +1,18 @@
+import Product from "@/data/model/Product";
+import Image from 'next/image';
+
+export interface ProductCardProps {
+   product: Product;
+};
+
+export default function ProductCard(props: ProductCardProps) {
+   const { name, description, price, image} = props.product;
+
+   return (
+      <div className="flex flex-col w-72">
+         <div className="relative w-72 h-52">
+            <Image src={image} alt={name} fill />
+         </div>
+      </div>
+   )
+};
