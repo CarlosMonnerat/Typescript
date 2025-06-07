27ec3c41b4d4ea985a6caaf5be431a5c2fd442f7
carlos <carlosh.monnerat@gmail.com>
2025-06-07 16:51:58 -0300

    update cards and template

--- React-NextJS/E-Commerce/ecommerce/src/components/product/ProductCard.tsx
+++ React-NextJS/E-Commerce/ecommerce/src/components/product/ProductCard.tsx
@@ -9,16 +9,16 @@ export default function ProductCard(props: ProductCardProps) {
    const { id, name, description, price, image} = props.product;
 
    return (
-      <div className="flex flex-col w-72 bg-zinc-800">
+      <div className="flex flex-col w-72 rounded-lg bg-zinc-800">
          <div className="relative w-72 h-52">
-            <Image src={image} alt={name} fill className="object-cover" />
+            <Image src={image} alt={name} fill className="object-cover rounded-t-lg" />
          </div>
          <div className="flex flex-col gap-4 p-5 flex-1  text-amber-100">
             <h2 className="text-xl font-bold ">{name}</h2>
             <p className="flex-1 text-sm">{description}</p>
             <div className="flex justify-between items-center">
                <span className="text-lg font-semibold mt-2">R${price.toFixed(2)}</span>
-               <button className="border rounded-full px-4 py-2">Adicionar</button>
+               <button className="border rounded-full px-5 py-2 text-sm cursor-pointer">Adicionar</button>
             </div>
          </div>
       </div>
