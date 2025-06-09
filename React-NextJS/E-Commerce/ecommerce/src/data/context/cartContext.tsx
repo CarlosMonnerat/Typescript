8670190380624406ef40c7cb42c6affb5c5c7a4d
carlos <carlosh.monnerat@gmail.com>
2025-06-09 18:06:08 -0300

    Update cartContext.tsx

--- React-NextJS/E-Commerce/ecommerce/src/data/context/cartContext.tsx
+++ React-NextJS/E-Commerce/ecommerce/src/data/context/cartContext.tsx
@@ -1,21 +1,32 @@
 import { createContext, useState } from "react";
+import itemCart from "../model/ItemCart";
+import Product from "../model/Product";
 
 interface cartContextProps {
-   number: number;
-   increment?: () => void;
-   decrement?: () => void;
+   items: itemCart[];
+   add: (item: Product) => void   
 };
 
 const cartContext = createContext<cartContextProps>({} as any);
 
 export function ProviderCart(props: any) {
-   const [numero, setNumero] = useState(100);
+   const [items, setItems] = useState<itemCart[]>([]);
+
+   function toAdd(product: Product) {
+      const index = items.findIndex((i) => i.product.id === product.id);
+      if(index === -1) {
+         setItems([...items, { product, quantity: 1 }]);
+      }else {
+         const newItems = [...items];
+         newItems[index].quantity++;
+         setItems(newItems);
+      };
+   };
    
    return(
       <cartContext.Provider value={{
-         number: numero,
-         increment: () => setNumero(numero + 1),
-         decrement: () => setNumero(numero - 1)
+         items,
+         toAdd,
       }}>
          {props.children}
       </cartContext.Provider>
